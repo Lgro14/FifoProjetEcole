70acc9b96eeb3a09f0e99dc80c2963c9a87c209b
Luís Gustavo OLIVEIRA <luisgustavo.oliveira@etu.emse.fr>
2026-04-30 10:12:33 +0200

    add_always_comb

--- sync_fifo.sv
+++ sync_fifo.sv
@@ -16,48 +16,75 @@ module sync_fifo #(
 
 logic [WIDTH-1:0] mem [0:DEPTH-1];
 
-localparam int nbit = DEPTH <= 1 ? 1 : $clog2(DEPTH) - 1;
+localparam int nbit = 
+    DEPTH <= 1 ? 1 :
+    $clog2(DEPTH) - 1;
 localparam int ncount = $clog2(DEPTH + 1) - 1;
 
 
-logic [nbit:0] wrptr;
-logic [nbit:0] rdptr;
-logic [ncount:0] count;
+logic [nbit:0] wrptr_q;
+logic [nbit:0] rdptr_q;
+logic [ncount:0] count_q;
 
-assign empty_o = (count == 0);
-assign full_o = (count == DEPTH);
-assign dout_o = mem[rdptr];
+logic [nbit:0] wrptr_d;
+logic [nbit:0] rdptr_d;
+logic [ncount:0] count_d;
+
+assign empty_o = (count_q == 0);
+assign full_o = (count_q == DEPTH);
+assign dout_o =
+    (empty_o && wr_en_i && rd_en_i) ? din_i :
+    empty_o ? '0 :
+    mem[rdptr_q];
+
+always_comb begin
+    wrptr_d = wrptr_q;
+    rdptr_d = rdptr_q;
+    count_d = count_q;
+
+    if (wr_en_i && rd_en_i) begin
+        if (!empty_o) begin
+            if (wrptr_q == DEPTH-1)
+                wrptr_d = '0;
+            else
+                wrptr_d = wrptr_q + 1;
+
+            if (rdptr_q == DEPTH-1)
+                rdptr_d = '0;
+            else
+                rdptr_d = rdptr_q + 1;
+        end
+        count_d = count_q;
+    end
+    else if (wr_en_i && !full_o) begin
+        if (wrptr_q == DEPTH-1)
+            wrptr_d = '0;
+        else
+            wrptr_d = wrptr_q + 1;
+        count_d = count_q + 1;
+
+    end
+    else if (rd_en_i && !empty_o) begin
+        if (rdptr_q == DEPTH-1)
+            rdptr_d = '0;
+        else
+            rdptr_d = rdptr_q + 1;
+        count_d = count_q - 1;
+    end
+end
 
 always_ff @(posedge clk) begin
     if (!rst_n) begin
-        wrptr <= '0;
-        rdptr <= '0;
-        count <= '0;
-    end else begin
-        if (wr_en_i && rd_en_i && !full_o && !empty_o) begin
-            mem[wrptr] <= din_i;
-            if (wrptr == DEPTH-1)
-                wrptr <= '0;
-            else
-                wrptr <= wrptr + 1;
-
-            if (rdptr == DEPTH-1)
-                rdptr <= '0; 
-            else
-                rdptr <= rdptr + 1;
-        end else if (wr_en_i && !full_o) begin
-            mem[wrptr] <= din_i;
-            if (wrptr == DEPTH-1)
-                wrptr <= '0;
-            else
-                wrptr <= wrptr + 1;
-            count <= count + 1;
-        end else if (rd_en_i && !empty_o) begin
-            if (rdptr == DEPTH-1)
-                rdptr <= '0;
-            else
-                rdptr <= rdptr + 1;
-            count <= count - 1;
+        wrptr_q <= '0;
+        rdptr_q <= '0;
+        count_q <= '0;
+    end
+    else begin
+        wrptr_q <= wrptr_d;
+        rdptr_q <= rdptr_d;
+        count_q <= count_d;
+        if (wr_en_i && ((!rd_en_i && !full_o) || (rd_en_i && !empty_o))) begin
+            mem[wrptr_q] <= din_i;
         end
     end
 end
